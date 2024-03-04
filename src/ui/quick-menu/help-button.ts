5a43e54bf9ea65308624bb424aca1ee6cf7b0df5
krypek <krypek@tuta.io>
2024-03-04 18:50:57 +0100

    Improve the quick menu help dialog

--- src/ui/quick-menu/help-button.ts
+++ src/ui/quick-menu/help-button.ts
@@ -1,7 +1,29 @@
 ig.module("nax-ccuilib.ui.quick-menu.help-button").defines(() => {
-	let button: sc.ButtonGui;
-	const localStorageId = "ccuilib-hidequickmenuhelpbutton";
+	/* it differs from sc.Dialogs.showChoiceDialog by returning the dialog instance */
+	function showChoiceDialog(
+		text: sc.TextLike,
+		icon: Nullable<sc.DIALOG_INFO_ICON>,
+		option: sc.TextLike[],
+		callback: (button: sc.ButtonGui & { data: number }, dialog?: sc.ModalButtonInteract) => void,
+		disableSubmitSound?: Nullable<boolean>
+	): sc.ModalButtonInteract {
+		const popup = new sc.ModalButtonInteract(text, icon, option, callback, disableSubmitSound);
+		ig.gui.addGuiElement(popup);
+		popup.show();
+		return popup;
+	}
 
+	let popup: sc.ModalButtonInteract;
+	/* force keep the menu open while the popup is visible */
+	sc.Control.inject({
+		quickmenu() {
+			if (popup?.hook?.currentStateName == "DEFAULT") return true;
+			return this.parent();
+		},
+	});
+
+	const localStorageId = "ccuilib-hidequickmenuhelpbutton";
+	let button: sc.ButtonGui;
 	sc.QuickMenu.inject({
 		init() {
 			this.parent();
@@ -16,7 +38,7 @@ ig.module("nax-ccuilib.ui.quick-menu.help-button").defines(() => {
 					"Press \\i[key-throw] or \\i[gamepad-x] on a focused button to \\c[3]cut\\c[0] it, then press \\i[key-throw] or \\i[gamepad-x] on a different button to \\c[3]paste\\c[0] it there.\n" +
 					"To switch rings on gamepad, use \\i[gamepad-l1] and \\i[gamepad-r1].\n" +
 					"Choose the \\c[3]Hide\\c[0] button to hide the help button.";
-				sc.Dialogs.showChoiceDialog(text, sc.DIALOG_INFO_ICON.INFO, ["Close", "Hide"], button => {
+				popup = showChoiceDialog(text, sc.DIALOG_INFO_ICON.INFO, ["Close", "Hide"], button => {
 					if (button.data == 1) {
 						localStorage.setItem(localStorageId, "true");
 					}
@@ -27,23 +49,10 @@ ig.module("nax-ccuilib.ui.quick-menu.help-button").defines(() => {
 		_enterMenu() {
 			this.parent();
 
+			sc.quickmodel.buttonInteract.addGlobalButton(button, () => sc.control.menuHotkeyHelp());
 			if (localStorage.getItem(localStorageId) != "true") {
-				sc.quickmodel.buttonInteract.addGlobalButton(button, () => sc.control.menuHotkeyHelp());
 				button.doStateTransition("DEFAULT");
-
 				button.doPosTranstition(349, 295, 0, KEY_SPLINES.LINEAR);
-			}
-		},
-		_exitMenu() {
-			this.parent();
-
-			if (button.hook.currentStateName == "DEFAULT") {
-				sc.quickmodel.buttonInteract.removeGlobalButton(button);
-				button.doStateTransition("HIDDEN", true);
-				const popup = ig.gui.guiHooks.find(h => h.gui instanceof sc.ModalButtonInteract)?.gui as sc.ModalButtonInteract | undefined;
-				if (popup) {
-					popup.buttongroup._invokePressCallbacks(popup.buttons[0]);
-				}
 			}
 		},
 	});
